fa112ac40d6ddde5eea03293735462f6187408fa
tanavinarkhede <narkhedetanavi21@gmail.com>
2024-04-24 13:52:36 +0530

    first commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,56 @@
+// import React from "react";
+
+// const NewsItem = (props) => {
+
+//     let {title, description, ImageUrl, newsUrl, author, date, } = props;
+//     return (
+//       <div className="my-3">
+//         <div className="card">
+        
+//           <img src={!ImageUrl?"https://i.ytimg.com/vi/WSb9xdUXKFA/maxresdefault.jpg":ImageUrl} className="card-img-top" alt="..." />
+//           <div className="card-body">
+//             <h5 className="card-title">{title}...
+//            </h5>
+//             <p className="card-text">{description}...</p>
+//             <p className="card-text"><small className="text-muted">By {!author? "Unknown":author} on {date}</small></p>
+//             <a rel="noreferrer" href={newsUrl} /*target="_blank"*/ className="btn btn-sm btn-dark">
+//               Read More
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//     );
+// }
+
+// export default NewsItem;
+
+import React from 'react'
+
+const NewsItem = (props)=> {
+        let { title, description, imageUrl, newsUrl, author, date, source } = props;
+        return (
+            <div className="my-3">
+                <div className="card">
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }
+                    }> 
+                        <span className="badge rounded-pill bg-danger"> {source} </span>
+                    </div>
+                    <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}  </h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+     
+}
+
+export default NewsItem
---
+++ src/components/Spinner.js
@@ -0,0 +1,27 @@
+// import React, { Component } from 'react'
+// import loading from './loading.gif'
+
+// const Spinner = () =>{
+
+//     return (
+//       <div className='text-center'>
+//         <img src={loading} alt="loading" />
+//       </div>
+//     )
+  
+// }
+
+// export default Spinner
+
+import React from 'react'
+import loading from './loading.gif'
+
+const Spinner = ()=> {
+        return (
+            <div className="text-center">
+                <img className="my-3" src={loading} alt="loading" />
+            </div>
+        )
+}
+
+export default Spinner
